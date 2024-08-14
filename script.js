a8b99631ec021c5e934a16320787966274b6934f
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-08-14 21:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
+function mincost(arr){
+let srt = arr.sort((a,b) => a-b);
+let jump =[];
+let first = srt[0] + srt[1];
+jump.push(first);
+
+
+for (let i = 2; i < arr.length; i++) {
   
+  let second = first + srt[i]
+   first=second;
+   jump.push(second)
+
+}
+
+let sum = 0
+for(let i = 0; i<jump.length;i++){
+  sum = jump[i] + sum
+}
+
+return(sum)
+
 }
 
 module.exports=mincost;
